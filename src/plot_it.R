46e186d47e5e938a9547127163d411ccd925c2c1
xxemmexx <emme@pm.me>
2024-09-08 01:10:05 +0200

    Migrated project to GitHub

---
+++ src/plot_it.R
@@ -0,0 +1,22 @@
+require(readxl)
+require(ggplot2)
+require(dplyr)
+require(stringr)
+
+indices_df <- readxl::read_xlsx('data/interim/indices_df.xlsx')
+
+indices_df |>
+  group_by(year) |>
+  summarise(n())
+
+indices_df |>
+  mutate(location = stringr::str_sub(loc_code, start = 2),
+         index = 1) |>
+  ggplot(aes(x = location, y = ADI, color = year)) +
+  geom_boxplot() +
+  ggtitle("Boxplots for habitat F3") +
+  xlab("Arbitrary index") + ylab("Acoustic diversity index (ADI)")
+
+
+ggsave('results/boxplots_F3.png')  
+
